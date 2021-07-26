17302adb1f83fe8547f1aa9d1f663c64645a49ca
nirmitchattoo <86514842+nirmitchattoo@users.noreply.github.com>
2021-07-27 03:42:17 +0530

    Create CodeChef-ColdPlay- SLOOP.cpp

---
+++ CodeChef-ColdPlay- SLOOP.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/SLOOP
+//Problem Code- SLOOP
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t>0){
+	    int M,S,n;
+	    cin>>M>>S;
+	    n=M/S;
+	    
+	    cout<<n<<'\n';
+	    t--;
+	}
+	return 0;
+}
