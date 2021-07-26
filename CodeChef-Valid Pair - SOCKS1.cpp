8b70cd5bca904f8deacc77c2cf745a5d4c8dc825
nirmitchattoo <86514842+nirmitchattoo@users.noreply.github.com>
2021-07-27 05:14:19 +0530

    Create CodeChef-Valid Pair - SOCKS1.cpp

---
+++ CodeChef-Valid Pair - SOCKS1.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/SOCKS1
+//Problem CODE-SOCKS1
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    
+	    if(a==b || b==c || c==a){
+	        cout<<"YES"<<'\n';
+	    }
+	    
+	    else{
+	        cout<<"NO"<<'\n';
+	    }
+	    
+	    
+	
+	return 0;
+}
