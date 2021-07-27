2c8f3c9399d610618e765e37b7c4ee14484a973d
nirmitchattoo <86514842+nirmitchattoo@users.noreply.github.com>
2021-07-27 13:07:51 +0530

    Create CodeChef-Hidden Numbers-UWCOI21A.cpp

---
+++ CodeChef-Hidden Numbers-UWCOI21A.cpp
@@ -0,0 +1,23 @@
+//codechef.com/problems/UWCOI21A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t!=0){
+	    int n;
+	    cin>>n;
+	    if(n==10){
+	        cout<<2<<" "<<5<<endl;
+	    }
+	    
+	    else{
+	        cout<<1<<" "<<n<<endl;
+	    }
+	    
+	    t--;
+	}
+	return 0;
+}
