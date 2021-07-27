d892cb32b8667019e875a06d0328c30a6968a93e
nirmitchattoo <86514842+nirmitchattoo@users.noreply.github.com>
2021-07-27 12:56:06 +0530

    Create CodeChef- Relativity-RELATIVE.cpp

---
+++ CodeChef- Relativity-RELATIVE.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/RELATIVE
+//Problem CODE-RELATIVE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t!=0){
+	    int g,c,h;
+	    cin>>g>>c;
+	    h=(c*c)/(2*g);
+	    cout<<h<<endl;
+	    t--;
+	    
+	}
+	return 0;
+}
